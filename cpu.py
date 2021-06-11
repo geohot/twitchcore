72ad4a52ae1b3dd98f79352243d16fa031cf5bcf
George Hotz <geohot@gmail.com>
2021-06-11 14:56:06 -0700

    work

--- cpu.py
+++ cpu.py
@@ -26,8 +26,8 @@ memory = None
 def reset():
   global regfile, memory
   regfile = Regfile()
-  # 64k at 0x80000000
-  memory = b'\x00'*0x10000
+  # 8kb at 0x80000000
+  memory = b'\x00'*0x2000
 
 from enum import Enum
 # RV32I Base Instruction Set
@@ -159,16 +159,15 @@ def cond(funct3, vs1, vs2):
   return ret
 
 def step():
-  # Instruction Fetch
+  # *** Instruction Fetch ***
   ins = r32(regfile[PC])
+
+  # *** Instruction decode and register fetch ***
   def gibi(s, e):
     return (ins >> e) & ((1 << (s-e+1))-1)
-
-  # Instruction decode and register fetch
   opcode = Ops(gibi(6, 0))
   funct3 = Funct3(gibi(14, 12))
   funct7 = gibi(31, 25)
-  npc = regfile[PC] + 4
   imm_i = sign_extend(gibi(31, 20), 12)
   imm_s = sign_extend(gibi(31, 25)<<5 | gibi(11, 7), 12)
   imm_b = sign_extend((gibi(32, 31)<<12) | (gibi(30, 25)<<5) | (gibi(11, 8)<<1) | (gibi(8, 7)<<11), 13)
@@ -181,35 +180,42 @@ def step():
   vpc = regfile[PC]
 
   # register write set up
-  rd = gibi(11, 7)
-  pend = None
-  #print("%x %8x %r" % (regfile[PC], ins, opcode))
+  rd = gibi(11, 7) if opcode != Ops.BRANCH else 0
+  reg_writeback = False
+  pend_is_new_pc = False
+  do_load = False
+  do_store = False
 
-  # Execute
+  # *** Execute ***
   if opcode == Ops.JAL:
     # J-type instruction
-    pend = vpc + 4
-    npc = vpc + imm_j
+    pend_is_new_pc = True
+    pend = vpc + imm_j
   elif opcode == Ops.JALR:
     # I-type instruction
-    pend = vpc + 4
-    npc = vs1 + imm_i
+    pend_is_new_pc = True
+    pend = vs1 + imm_i
   elif opcode == Ops.BRANCH:
     # B-type instruction
     if cond(funct3, vs1, vs2):
-      npc = vpc + imm_b
+      pend_is_new_pc = True
+      pend = vpc + imm_b
   elif opcode == Ops.AUIPC:
     # U-type instruction
     pend = arith(Funct3.ADD, vpc, imm_u, False)
+    reg_writeback = True
   elif opcode == Ops.LUI:
     # U-type instruction
     pend = imm_u
+    reg_writeback = True
   elif opcode == Ops.OP:
     # R-type instruction
     pend = arith(funct3, vs1, vs2, funct7 == 0b0100000)
+    reg_writeback = True
   elif opcode == Ops.IMM:
     # I-type instruction
     pend = arith(funct3, vs1, imm_i, funct7 == 0b0100000 and funct3 == Funct3.SRAI)
+    reg_writeback = True
   elif opcode == Ops.MISC:
     pass
   elif opcode == Ops.SYSTEM:
@@ -224,34 +230,45 @@ def step():
   # Memory access step
   elif opcode == Ops.LOAD:
     # I-type instruction
-    addr = vs1 + imm_i
-    if funct3 == Funct3.LB:
-      pend = sign_extend(r32(addr)&0xFF, 8)
-    elif funct3 == Funct3.LH:
-      pend = sign_extend(r32(addr)&0xFFFF, 16)
-    elif funct3 == Funct3.LW:
-      pend = r32(addr)
-    elif funct3 == Funct3.LBU:
-      pend = r32(addr)&0xFF
-    elif funct3 == Funct3.LHU:
-      pend = r32(addr)&0xFFFF
+    pend = vs1 + imm_i
+    do_load = True
+    reg_writeback = True
   elif opcode == Ops.STORE:
     # S-type instruction
-    addr = vs1 + imm_s
-    if funct3 == Funct3.SB:
-      ws(addr, struct.pack("B", vs2&0xFF))
-    elif funct3 == Funct3.SH:
-      ws(addr, struct.pack("H", vs2&0xFFFF))
-    elif funct3 == Funct3.SW:
-      ws(addr, struct.pack("I", vs2))
+    pend = vs1 + imm_s
+    do_store = True
   else:
     dump()
     raise Exception("write op %r" % opcode)
 
-  # Register write back
-  if pend is not None:
-    regfile[rd] = pend
-  regfile[PC] = npc
+  # *** Memory access ***
+  if do_load:
+    if funct3 == Funct3.LB:
+      pend = sign_extend(r32(pend)&0xFF, 8)
+    elif funct3 == Funct3.LH:
+      pend = sign_extend(r32(pend)&0xFFFF, 16)
+    elif funct3 == Funct3.LW:
+      pend = r32(pend)
+    elif funct3 == Funct3.LBU:
+      pend = r32(pend)&0xFF
+    elif funct3 == Funct3.LHU:
+      pend = r32(pend)&0xFFFF
+  elif do_store:
+    if funct3 == Funct3.SB:
+      ws(pend, struct.pack("B", vs2&0xFF))
+    elif funct3 == Funct3.SH:
+      ws(pend, struct.pack("H", vs2&0xFFFF))
+    elif funct3 == Funct3.SW:
+      ws(pend, struct.pack("I", vs2))
+
+  # *** Register write back ***
+  if pend_is_new_pc:
+    regfile[rd] = vpc + 4
+    regfile[PC] = pend
+  else:
+    if reg_writeback:
+      regfile[rd] = pend
+    regfile[PC] = vpc + 4
   return True
 
 
